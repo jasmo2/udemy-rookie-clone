39bb8bedb909fa5f7e73d9afbea4e9c2e5dc9822
jasmo2 <jasmo2.com@gmail.com>
2015-03-02 12:45:39 -0500

    delete remotetly and localy migrations files

--- app/models/session.rb
+++
@@ -1,4 +0,0 @@
-class Session < ActiveRecord::Base
-	belongs_to :owner, class_name: "User", foreign_key: "user_id"
-	belongs_to :course
-end
--- db/migrate/20150225200643_create_sessions.rb
+++
@@ -1,9 +0,0 @@
-class CreateSessions < ActiveRecord::Migration
-  def change
-    create_table :sessions do |t|
-    	t.integer :user_id
-    	t.integer :course_id
-      t.timestamps null: false
-    end
-  end
-end
